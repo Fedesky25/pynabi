00f18e745be45b722f0d82a0053f3f38c8b3d6d3
Fedesky25 <fedesky.guglielmi@gmail.com>
2023-06-06 20:29:31 +0200

    refactor: units

--- pynabi/units.py
+++ pynabi/units.py
@@ -1,85 +1,112 @@
-from enum import Enum as _E
-from typing import Optional as _O
-from ._common import OneLineStamp as _OLS
+from typing import Union, Optional, Tuple, Self, Any
 
+class _AbMeasure:
+    _U: Tuple[Tuple[float, str],...] = ()
+    _R = (1.0, 0)
 
-__all__ = ["LUnit", "Pos3D", "Length", "EUnit", "EUnit"]
+    def __init__(self, value: float, unit: int) -> None:
+        """Do not use this constructor"""
+        self._v = value
+        self._u = unit
 
+    def __mul__(self, other: Union[int,float]) -> Self:
+        if type(other) is int or type(other) is float:
+            return type(self)(self._v*other, self._u) # type: ignore
+        else:
+            return NotImplemented
 
-class LUnit(_E):
-    Bohr = 0
-    nm = 1
-    Angstrom = 2
+    def __rmul__(self, other: Union[int,float]) -> Self:
+        return type(self).__mul__(self, other)
 
-    def setAsDefault(self):
-        global _def_LU
-        _def_LU = self
+    def __add__(self, other: Self):
+        if type(other) is not Length:
+            return NotImplemented
+        U = type(self)._U
+        delta = other._v * U[other._u][0] / U[self._u][0]
+        return type(self)(self._v + delta, self._u);
+
+    def __sub__(self, other: Self) -> 'Self':
+        if type(other) is not Length:
+            return NotImplemented # type: ignore
+        U = type(self)._U
+        delta = other._v * U[other._u][0] / U[self._u][0]
+        return type(self)(self._v - delta, self._u);
+
+    def __neg__(self):
+        return type(self)(-self._v, self._u)
+
+    def __str__(self):
+        return f"{self._v} {type(self)._U[self._u][0]}"
     
-    @staticmethod
-    def getDefault():
-        return _def_LU
+    def setAsReference(self):
+        type(self)._R = (self._v, self._u)
+    
+    @classmethod
+    def fromReference(cls, value: float, ref: Optional[Self]) -> Self:
+        assert type(value) is float or type(value) is int, f"Value of {cls.__name__} must be a number"
+        if ref is None:
+            return cls(value*cls._R[0], cls._R[1])
+        else:
+            assert type(ref) is cls, f"Reference for a {cls.__name__} mus be a {cls.__name__} itself"
+            return value*ref
 
-_def_LU = LUnit.Bohr
+    @classmethod
+    def sanitize(cls, value: Any) -> Self:
+        t = type(value)
+        if t is float or t is int:
+            return cls(value*cls._R[0], cls._R[1])
+        elif t is cls:
+            return value
+        else:
+            raise TypeError(f"Cannot construct {cls.__name__} from {value} ({t})")
+        
+    @classmethod
+    def getDefaultReference(cls):
+        return cls(cls._R[0], cls._R[1])
+
+
+class Length(_AbMeasure):
+    _U = ((0.529177249, "Bohr"), (1.0, "Angstrom"), (10.0, "nm"))
+
+
+Bohr = Length(1.0, 0)
+Ang = Length(1.0, 1)
+nm = Length(1.0, 2)
+
+
+class Energy(_AbMeasure):
+    _U = (
+        (27.2114, "Ha"),
+        (1.0, "eV"),
+        (13.6057039763, "Ry"),
+        (8.617328149741e-5, "K")
+    )
+
+
+Ha = Energy(1.0, 0)
+eV = Energy(1.0, 1)
+Ry = Energy(1.0, 2)
+Kelvin = Energy(1.0, 3)
 
 
 class Pos3D:
-    def __init__(self, x: float, y: float, z: float, unit: _O[LUnit] = None) -> None:
-        assert type(unit) is LUnit, f"{unit} is not valid length unit"
-        self.x = x
-        self.y = y
-        self.z = z
+    def __init__(self, x: float, y: float, z: float, unit: Optional[Length] = None) -> None:
+        m = 1.0
         if unit is None:
-            self.u = _def_LU
+            m = Length._R[0]
+            self.u = Length._R[1]
+        elif type(unit) is Length:
+            m = unit._v
+            self.u = unit._u
         else:
-            assert type(unit) is LUnit, f"{unit} is not  avlid length unit"
-            self.u = unit
+            raise TypeError("3D Position reference must be a Length itself")
+        self.x = x*m
+        self.y = y*m
+        self.z = z*m
     
     def __str__(self) -> str:
-        return f"{self.x} {self.y} {self.z} {self.u.name}"
-    
-    @staticmethod
-    def uniform(l: 'Length'):
-        return Pos3D(l._v,l._v,l._v,l._u) # type: ignore
-
-
-class Length:
-    def __init__(self, value: float, unit: _O[LUnit] = None) -> None:
-        self._v = value
-        if unit is None:
-            self._u = _def_LU
-        else:
-            assert type(unit) is LUnit, f"{unit} is not  avlid length unit"
-            self._u = unit
-
-    def __str__(self) -> str:
-        return f"{self._v} {self._u.name}"
-
-
-class EUnit(_E):
-    Ha = 0
-    eV = 1
-    Ry = 3
-    K = 4
-
-    def setAsDefault(self):
-        global _def_EU
-        _def_EU = self
+        return f"{self.x} {self.y} {self.z} {Length._U[self.u][1]}"
     
     @staticmethod
-    def getDefault():
-        return _def_EU
-
-_def_EU = EUnit.Ha
-
-
-class Energy:
-    def __init__(self, value: float, unit: _O[EUnit] = None) -> None:
-        self._v = value
-        if unit is None:
-            self._u = _def_EU
-        else:
-            assert type(unit) is EUnit, f"{unit} is not valid energy unit"
-            self._u = unit
-
-    def __str__(self) -> str:
-        return f"{self._v} {self._u.name}"
+    def uniform(l: Union[float,'Length']):
+        return Pos3D(1,1,1,Length.sanitize(l))
