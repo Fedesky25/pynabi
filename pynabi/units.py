8aedbf5be7d14c1a21a103acb64b72cf4620213f
Fedesky25 <fedesky.guglielmi@gmail.com>
2023-06-08 13:00:02 +0200

    chore: typing import with leading _

--- pynabi/units.py
+++ pynabi/units.py
@@ -1,7 +1,7 @@
-from typing import Union, Optional, Tuple, Self, Any
+from typing import Union as _Un, Optional as _Op, Tuple as _Tu, Self as  _Self, Any as _Any
 
 class _AbMeasure:
-    _U: Tuple[Tuple[float, str],...] = ()
+    _U: _Tu[_Tu[float, str],...] = ()
     _R = (1.0, 0)
 
     def __init__(self, value: float, unit: int) -> None:
@@ -9,23 +9,23 @@ class _AbMeasure:
         self._v = value
         self._u = unit
 
-    def __mul__(self, other: Union[int,float]) -> Self:
+    def __mul__(self, other: _Un[int,float]) -> _Self:
         if type(other) is int or type(other) is float:
             return type(self)(self._v*other, self._u) # type: ignore
         else:
             return NotImplemented
 
-    def __rmul__(self, other: Union[int,float]) -> Self:
+    def __rmul__(self, other: _Un[int,float]) -> _Self:
         return type(self).__mul__(self, other)
 
-    def __add__(self, other: Self):
+    def __add__(self, other: _Self):
         if type(other) is not Length:
             return NotImplemented
         U = type(self)._U
         delta = other._v * U[other._u][0] / U[self._u][0]
         return type(self)(self._v + delta, self._u);
 
-    def __sub__(self, other: Self) -> 'Self':
+    def __sub__(self, other: _Self) -> '_Self':
         if type(other) is not Length:
             return NotImplemented # type: ignore
         U = type(self)._U
@@ -42,7 +42,7 @@ class _AbMeasure:
         type(self)._R = (self._v, self._u)
     
     @classmethod
-    def fromReference(cls, value: float, ref: Optional[Self]) -> Self:
+    def fromReference(cls, value: float, ref: _Op[_Self]) -> _Self:
         assert type(value) is float or type(value) is int, f"Value of {cls.__name__} must be a number"
         if ref is None:
             return cls(value*cls._R[0], cls._R[1])
@@ -51,7 +51,7 @@ class _AbMeasure:
             return value*ref
 
     @classmethod
-    def sanitize(cls, value: Any) -> Self:
+    def sanitize(cls, value: _Any) -> _Self:
         t = type(value)
         if t is float or t is int:
             return cls(value*cls._R[0], cls._R[1])
@@ -90,7 +90,7 @@ Kelvin = Energy(1.0, 3)
 
 
 class Pos3D:
-    def __init__(self, x: float, y: float, z: float, unit: Optional[Length] = None) -> None:
+    def __init__(self, x: float, y: float, z: float, unit: _Op[Length] = None) -> None:
         m = 1.0
         if unit is None:
             m = Length._R[0]
@@ -108,5 +108,5 @@ class Pos3D:
         return f"{self.x} {self.y} {self.z} {Length._U[self.u][1]}"
     
     @staticmethod
-    def uniform(l: Union[float,'Length']):
+    def uniform(l: _Un[float,'Length']):
         return Pos3D(1,1,1,Length.sanitize(l))
